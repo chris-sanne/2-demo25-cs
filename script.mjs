23943afdd53b7a59abafe7163b1af44eb5107d3e
Christoffer <christofas@uia.no>
2025-01-25 03:35:58 +0100

    Added functionality for shuffling a deck as well as retrieving the deck.

--- script.mjs
+++ script.mjs
@@ -39,6 +39,23 @@ function generateDeck() {
         .then(cardDeck => console.log(cardDeck));
 }
 
+function shuffleDeck(deck) {
+    const shuffledDeck = {};
+
+    for (const suit in deck) {
+        const suitCards = [...deck[suit]];
+        const shuffledSuit = [];
+
+        while (suitCards.length > 0) {
+            const randomIndex = Math.floor(Math.random() * suitCards.length);
+            const card = suitCards.splice(randomIndex, 1)[0];
+            shuffledSuit.push(card);
+        }
+        shuffledDeck[suit] = shuffledSuit;
+    }
+    return shuffledDeck;
+}
+
 server.get("/", getRoot);
 
 server.post("/temp/deck", (req, res, next) => {
@@ -49,6 +66,36 @@ server.post("/temp/deck", (req, res, next) => {
     console.log(decks[deck_id]);
 
     res.status(HTTP_CODES.SUCCESS.OK).send().end();
+});
+
+server.patch("/temp/deck/shuffle/:deck_id", (req, res, next) => {
+    const deck_id = req.params.deck_id;
+
+    if (!decks[deck_id]) {
+        return res.status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND).send("No deck found").end();
+    }
+
+    const shuffledDeck = shuffleDeck(decks[deck_id]);
+
+    decks[deck_id] = shuffledDeck;
+    
+    console.log(`Deck with ID: ${deck_id} shuffled`);
+    console.log(decks[deck_id]);
+
+    res.status(HTTP_CODES.SUCCESS.OK).json(shuffledDeck).end();
+})
+
+server.get("/temp/deck/:deck_id", (req, res, next) => {
+    const deck_id = req.params.deck_id;
+
+    if (decks[deck_id]) {
+        console.log(`Deck with ID: ${deck_id} retrieved`);
+        console.log(decks[deck_id]);
+        
+        res.status(HTTP_CODES.SUCCESS.OK).json(decks[deck_id]).end();
+    } else {
+        res.status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND).send("No deck found").end();
+    }
 });
 
 server.listen(server.get('port'), function () {
