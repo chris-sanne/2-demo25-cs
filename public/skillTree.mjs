f66123695030a7d3ed96a3ce09f4811d136677a4
Christoffer <christofas@uia.no>
2025-03-12 05:35:41 +0100

    Added feature to display skill tree on the page back in from an older commit.

--- public/skillTree.mjs
+++ public/skillTree.mjs
@@ -9,6 +9,20 @@ const btnGetSkillTree = document.getElementById("btnGetSkillTree");
 const inputSkillName = document.getElementById("inputSkillName");
 const btnUnlockSkill = document.getElementById("btnUnlockSkill");
 const btnDeleteSkill = document.getElementById("btnDeleteSkill");
+const skillTreeElement = document.getElementById("skillTreeElement");
+
+function showSkillTree(skill, parentElement) {
+  const listItem = document.createElement("li");
+  listItem.innerText = skill.name + (skill.unlocked ? " ✅ " : " ❌ ");
+  listItem.id = skill.name.replaceAll(" ", "-");
+  parentElement.appendChild(listItem);
+  
+  if (skill.subskills.length > 0) {
+    const unorderedList = document.createElement("ul");
+    listItem.appendChild(unorderedList);
+    skill.subskills.forEach(subskill => showSkillTree(subskill, unorderedList));
+  }
+}
 
 btnNewSkillTree.addEventListener("click", async () => {
   const response = await fetch("/", { method: "POST" });
@@ -23,6 +37,8 @@ btnGetSkillTree.addEventListener("click", async () => {
     const data = await response.json();
     console.log("My skill tree requested from the server.");
     console.log(data);
+    skillTreeElement.innerHTML = "";
+    showSkillTree(data, skillTreeElement);
   } catch (error) {
     console.error("Error parsing JSON:", error)
   }
@@ -34,6 +50,10 @@ btnUnlockSkill.addEventListener("click", async () => {
   try {
     const data = await response.json();
     console.log("Attempting to update skill...", data);
+    const skillTreeResponse = await fetch("/api/skill-tree", { method: "GET" });
+    const skillTreeData = await skillTreeResponse.json();
+    skillTreeElement.innerHTML = "";
+    showSkillTree(skillTreeData, skillTreeElement);
   } catch (error) {
     console.error("Error updating skill", error);
   }
@@ -45,6 +65,10 @@ btnDeleteSkill.addEventListener("click", async () => {
   try {
     const data = await response.json();
     console.log("Attempting to delete skill...", data);
+    const skillTreeResponse = await fetch("/api/skill-tree", { method: "GET" });
+    const skillTreeData = await skillTreeResponse.json();
+    skillTreeElement.innerHTML = "";
+    showSkillTree(skillTreeData, skillTreeElement);
   } catch (error) {
     console.error("Error updating skill", error);
   }
